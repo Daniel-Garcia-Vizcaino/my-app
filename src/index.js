bbf2e1fedb072d6d3e0b2dbd44ff61370060884c
Daniel Garcia Vizcaino <danielgarciavizcaino@gmail.com>
2022-10-26 16:40:09 +0200

    Calculate winner

--- src/index.js
+++ src/index.js
@@ -41,7 +41,13 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'The winner is: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -80,6 +86,27 @@ import './index.css';
         </div>
       );
     }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const times = lines.length
+    for( let i = 0; i < times; i++ ) {
+      const [ a, b, c ] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
